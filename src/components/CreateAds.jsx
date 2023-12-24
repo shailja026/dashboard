39df20428288d2c2d89aea40f9db003f80657b38
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-12-24 13:59:00 +0530

    create ad section done

--- src/components/CreateAds.jsx
+++ src/components/CreateAds.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import styles from "../styles/createads.module.css"
+import React, { useState } from "react";
+import styles from "../styles/createads.module.css";
+import adMedia from "../assets/meadiAd.png";
+import textMedia from "../assets/adText.png";
 function CreateAds() {
+  const [checkedText, setCheckedText] = useState(false);
+  const [checkedMedia, setCheckedMedia] = useState(false);
+
+  const handleChangeText = () => {
+    setCheckedText(!checkedText);
+  };
+  const handleChangeMedia = () => {
+    setCheckedMedia(!checkedMedia);
+  };
   return (
     <div className={styles.adsSections}>
       <p>Create ads</p>
-      <div>
-        
+      <div className={styles.adBox}>
+        <div className={styles.adversite}>
+          <input
+            type="checkbox"
+            checked={checkedText}
+            onChange={handleChangeText}
+            className={styles.checkbox}
+          />
+          <img src={textMedia} alt="" />
+          <p>create</p>
+          <button>Text Ad</button>
+        </div>
+        <div className={styles.adversite}>
+          <input
+            type="checkbox"
+            checked={checkedMedia}
+            onChange={handleChangeMedia}
+            className={styles.checkbox}
+          />
+          <img src={adMedia} alt="" />
+          <p>create</p>
+          <button>Media Ad</button>
+        </div>
       </div>
-      <button>Next</button>
-        
+      <button >Next</button>
     </div>
-  )
+  );
 }
 
-export default CreateAds
+export default CreateAds;
